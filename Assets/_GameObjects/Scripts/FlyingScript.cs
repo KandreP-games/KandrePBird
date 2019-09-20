442f698ecc0c8342baa99e69d97f8f8ef2ebc1f0
enrique <enrique.tunity@gmail.com>
2019-09-20 13:58:01 +0200

    20/09 Tuberías y Pájaro
    Yeah bro

---
+++ Assets/_GameObjects/Scripts/FlyingScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingScript : MonoBehaviour
+{
+    Rigidbody rb;
+    public int fuerza = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Saltar();
+        }
+    }
+    void Saltar()
+    {
+        rb.AddForce(Vector3.up * fuerza);
+    }
+}
---
+++ Assets/_GameObjects/Scripts/PipeSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    public GameObject prefabPipe;
+    void Start()
+    {
+        InvokeRepeating("SpawnPipe", 0, 2); //Cada dos segundos llama a la función SpawnPipe
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void SpawnPipe()
+    {
+        Instantiate(prefabPipe, transform);    
+    }
+}
